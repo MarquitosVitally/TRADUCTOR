ed2b625cbd497080b99fdbd6c3b71a4ecc8e010d
Marquitos <marquitosbbb19@gmail.com>
2025-12-10 04:42:23 -0600

    Transcriber

--- SRC/audio/transcriber.py
+++ SRC/audio/transcriber.py
@@ -1,57 +1,26 @@
-from faster_whisper import WhisperModel
-import ffmpeg
-import os
+# {} [ ] \
+import speech_recognition as sr
 
-def extract_audio(input_file, output_file="temp_audio.wav"):
-    """
-    Extrae el audio de cualquier archivo de video usando FFmpeg.
-    """
-    (
-        ffmpeg
-        .input(input_file)
-        .output(output_file, ac=1, ar="16000")
-        .overwrite_output()
-        .run(quiet=True)
-    )
-    return output_file
+filename ="audio_file.wav"
+output_txt ="transcription.txt"
 
+r = sr.Recognizer()
 
-def transcribe_audio(audio_path, model_size="small"):
-    """
-    Transcribe audio y detecta idioma automáticamente.
-    """
-    print("Cargando modelo Whisper...")
-    model = WhisperModel(model_size, device="cpu", compute_type="float32")
-
-    print("Transcribiendo audio...")
-    segments, info = model.transcribe(audio_path, beam_size=5)
-
-    print(f"Idioma detectado: {info.language}")
-
-    text = ""
-    for seg in segments:
-        text += seg.text + " "
-
-    return text.strip()
-
-
-def convert_media_to_text(file_path):
-    """
-    Función principal que convierte cualquier audio/video a texto.
-    """
-    print("Extrayendo audio...")
-    audio = extract_audio(file_path)
-
-    text = transcribe_audio(audio)
-
-    os.remove(audio)  # Limpia el archivo temporal
-
-    return text
-
-
-if __name__ == "__main__":
-    ruta = input("Ingresa la ruta del archivo de audio o video: ")
-    resultado = convert_media_to_text(ruta)
-
-    print("\n===== RESULTADO =====\n")
-    print(resultado)
+try:
+    with sr.AudioFile(filename) as source:
+        duration = int(source.DURATION)
+        fill_transcription = ""
+        print ("Procesando el archivo de audio...")
+        for i in range(0, duration, 10):
+            try:
+                audio_data = r.record(source, duration = 10)
+                text = r.recognize_google(audio_data, lenguage="es-ES")
+                full_transcription += text + "\n"
+                print(f"Fragmento {i// 10 + 1} : {text}")
+            except sr.UnknownValueError:
+                print(f"Fragmento {i // 10 + 1}: No se pudo entender el audio " )
+                full_transcription +="[No se pudo entender el audio]\n"
+            except sr.RequestError as e:
+                print(f"Error al comunicarse con el servidor de google: {e} ")
+                break
+        wite open (output_txt, "w", encoding=ut)
